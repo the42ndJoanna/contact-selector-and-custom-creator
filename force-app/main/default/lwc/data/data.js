ff8d7acc47cd01349ea206e15a1dcd81a05a39ab
Zhihan Huang <thoughtworks.huang@mercedes-benz.com>
2024-05-02 19:35:31 +0800

    update mock data

--- force-app/main/default/lwc/data/data.js
+++ force-app/main/default/lwc/data/data.js
@@ -1,78 +1,135 @@
-export const contacts = [{
-    "fields": {
-        "Name": {
-            "displayValue": "Rose Gonzalez",
-            "value": "Rose Gonzalez"
-        },
-        "Title": {
-            "displayValue": "SVP, Procurement",
-            "value": "SVP, Procurement"
-        },
-        "Email": {
-            "displayValue": "rose@edge.com",
-            "value": "rose@edge.com"
-        },
-        "Department": {
-            "displayValue": "Procurement",
-            "value": "Procurement"
-        },
-        "Id": {
-            "displayValue": "003aj00000215HRAAY",
-            "value": "003aj00000215HRAAY"
-        }
+export const contacts = [
+    {
+        "Id": "003aj00000215HRAAY",
+        "Name": "Rose Gonzalez",
+        "Title": "SVP Procurement",
+        "Email": "rose@edge.com",
+        "Department": "Procurement"
     },
-    "id": "003aj00000215HRAAY",
-},
-{
-    "fields": {
-        "Name": {
-            "displayValue": "Sean Forbes",
-            "value": "Sean Forbes"
-        },
-        "Title": {
-            "displayValue": "CFO",
-            "value": "CFO"
-        },
-        "Email": {
-            "displayValue": "sean@edge.com",
-            "value": "sean@edge.com"
-        },
-        "Department": {
-            "displayValue": "Finance",
-            "value": "Finance"
-        },
-        "Id": {
-            "displayValue": "003aj00000215HSAAY",
-            "value": "003aj00000215HSAAY"
-        }
+    {
+        "Id": "003aj00000215HSAAY",
+        "Name": "Sean Forbes",
+        "Title": "CFO",
+        "Email": "sean@edge.com",
+        "Department": "Finance"
     },
-    "id": "003aj00000215HSAAY",
-},
-{
-    "fields": {
-        "Name": {
-            "displayValue": "Jack Rogers",
-            "value": "Jack Rogers"
-        },
-        "Title": {
-            "displayValue": "VP, Facilities",
-            "value": "VP, Facilities"
-        },
-        "Email": {
-            "displayValue": "jrogers@burlington.com",
-            "value": "jrogers@burlington.com"
-        },
-        "Department": {
-            "displayValue": null,
-            "value": null
-        },
-        "Id": {
-            "displayValue": "003aj00000215HTAAY",
-            "value": "003aj00000215HTAAY",
-        }
+    {
+        "Id": "003aj00000215HTAAY",
+        "Name": "Jack Rogers",
+        "Title": "VP, Facilities",
+        "Email": "jrogers@burlington.com"
     },
-    "id": "003aj00000215HTAAY"
-}
-];
-
-
+    {
+        "Id": "003aj00000215HUAAY",
+        "Name": "Pat Stumuller",
+        "Title": "SVP, Administration and Finance",
+        "Email": "pat@pyramid.net",
+        "Department": "Finance"
+    },
+    {
+        "Id": "003aj00000215HVAAY",
+        "Name": "Andy Young",
+        "Title": "SVP, Operations",
+        "Email": "a_young@dickenson.com",
+        "Department": "Internal Operations"
+    },
+    {
+        "Id": "003aj00000215HWAAY",
+        "Name": "Tim Barr",
+        "Title": "SVP, Administration and Finance",
+        "Email": "barr_tim@grandhotels.com",
+        "Department": "Finance"
+    },
+    {
+        "Id": "003aj00000215HXAAY",
+        "Name": "John Bond",
+        "Title": "VP, Facilities",
+        "Email": "bond_john@grandhotels.com",
+        "Department": "Facilities"
+    },
+    {
+        "Id": "003aj00000215HYAAY",
+        "Name": "Stella Pavlova",
+        "Title": "SVP, Production",
+        "Email": "spavlova@uog.com",
+        "Department": "Production"
+    },
+    {
+        "Id": "003aj00000215HZAAY",
+        "Name": "Lauren Boyle",
+        "Title": "SVP, Technology",
+        "Email": "lboyle@uog.com",
+        "Department": "Technology"
+    },
+    {
+        "Id": "003aj00000215HaAAI",
+        "Name": "Babara Levy",
+        "Title": "SVP, Operations",
+        "Email": "b.levy@expressl&t.net",
+        "Department": "Operations"
+    },
+    {
+        "Id": "003aj00000215HbAAI",
+        "Name": "Josh Davis",
+        "Title": "Director, Warehouse Mgmt",
+        "Email": "j.davis@expressl&t.net",
+        "Department": "Warehouse Mgmt"
+    },
+    {
+        "Id": "003aj00000215HcAAI",
+        "Name": "Jane Grey",
+        "Title": "Dean of Administration",
+        "Email": "jane_gray@uoa.edu",
+        "Department": "Administration"
+    },
+    {
+        "Id": "003aj00000215HdAAI",
+        "Name": "Arthur Song",
+        "Title": "CEO",
+        "Email": "asong@uog.com",
+        "Department": "Executive Team"
+    },
+    {
+        "Id": "003aj00000215HeAAI",
+        "Name": "Ashley James",
+        "Title": "VP, Finance",
+        "Email": "ajames@uog.com",
+        "Department": "Finance"
+    },
+    {
+        "Id": "003aj00000215HfAAI",
+        "Name": "Tom Ripley",
+        "Title": "Regional General Manager",
+        "Email": "tripley@uog.com",
+        "Department": "Executive Team"
+    },
+    {
+        "Id": "003aj00000215HgAAI",
+        "Name": "Liz D'Cruz",
+        "Title": "VP, Production",
+        "Email": "ldcruz@uog.com",
+        "Department": "Production"
+    },
+    {
+        "Id": "003aj00000215HhAAI",
+        "Name": "Edna Frank",
+        "Title": "VP, Technology",
+        "Email": "efrank@genepoint.com",
+        "Department": "Technology"
+    },
+    {
+        "Id": "003aj00000215HiAAI",
+        "Name": "Avi Green",
+        "Title": "CFO",
+        "Email": "agreen@uog.com",
+        "Department": "Finance"
+    },
+    {
+        "Id": "003aj00000215HjAAI",
+        "Name": "Siddartha Nedaerk"
+    },
+    {
+        "Id": "003aj00000215HkAAI",
+        "Name": "Jake Llorrac"
+    }
+]
