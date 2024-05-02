2dd6ea14bb78e6466a7f3752739b7dee61bbe8cb
Zhihan Huang <thoughtworks.huang@mercedes-benz.com>
2024-05-02 19:36:39 +0800

    get contacts data and render tile list

--- force-app/main/default/lwc/list/list.js
+++ force-app/main/default/lwc/list/list.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
 
-export default class List extends LightningElement {}
+export default class List extends LightningElement {
+    contacts = [];
+    ready = false;
+    @wire(getContacts, {}) contacts({ data, error }) {
+        if(data) {
+          this.contacts = data;
+          this.ready = true;
+        }
+      }
+}
--- force-app/main/default/lwc/tile/tile.js
+++ force-app/main/default/lwc/tile/tile.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Tile extends LightningElement {}
+export default class Tile extends LightningElement {
+    @api contact;
+}
