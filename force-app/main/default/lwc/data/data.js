5622b96d72c634795d6637dc323be9baad35a870
Zhihan Huang <thoughtworks.huang@mercedes-benz.com>
2024-05-02 16:28:39 +0800

    mock contacts data

---
+++ force-app/main/default/lwc/data/data.js
@@ -0,0 +1,78 @@
+export const contacts = [{
+    "fields": {
+        "Name": {
+            "displayValue": "Rose Gonzalez",
+            "value": "Rose Gonzalez"
+        },
+        "Title": {
+            "displayValue": "SVP, Procurement",
+            "value": "SVP, Procurement"
+        },
+        "Email": {
+            "displayValue": "rose@edge.com",
+            "value": "rose@edge.com"
+        },
+        "Department": {
+            "displayValue": "Procurement",
+            "value": "Procurement"
+        },
+        "id": {
+            "displayValue": "003aj00000215HRAAY",
+            "value": "003aj00000215HRAAY"
+        }
+    },
+    "id": "003aj00000215HRAAY",
+},
+{
+    "fields": {
+        "Name": {
+            "displayValue": "Sean Forbes",
+            "value": "Sean Forbes"
+        },
+        "Title": {
+            "displayValue": "CFO",
+            "value": "CFO"
+        },
+        "Email": {
+            "displayValue": "sean@edge.com",
+            "value": "sean@edge.com"
+        },
+        "Department": {
+            "displayValue": "Finance",
+            "value": "Finance"
+        },
+        "id": {
+            "displayValue": "003aj00000215HSAAY",
+            "value": "003aj00000215HSAAY"
+        }
+    },
+    "id": "003aj00000215HSAAY",
+},
+{
+    "fields": {
+        "Name": {
+            "displayValue": "Jack Rogers",
+            "value": "Jack Rogers"
+        },
+        "Title": {
+            "displayValue": "VP, Facilities",
+            "value": "VP, Facilities"
+        },
+        "Email": {
+            "displayValue": "jrogers@burlington.com",
+            "value": "jrogers@burlington.com"
+        },
+        "Department": {
+            "displayValue": null,
+            "value": null
+        },
+        "id": {
+            "displayValue": "003aj00000215HTAAY",
+            "value": "003aj00000215HTAAY",
+        }
+    },
+    "id": "003aj00000215HTAAY"
+}
+];
+
+
