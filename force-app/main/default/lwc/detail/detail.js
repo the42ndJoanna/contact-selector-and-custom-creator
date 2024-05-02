d29ea8878d7f119eb9a083ea805bd32947b6d7b1
Zhihan Huang <thoughtworks.huang@mercedes-benz.com>
2024-05-02 21:10:38 +0800

    selector communicate with detail by messageService

--- force-app/main/default/lwc/detail/detail.js
+++ force-app/main/default/lwc/detail/detail.js
@@ -1,5 +1,20 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import CONTACT_SELECTED_CHANNEL from '@salesforce/messageChannel/Contact_Selected__c';
 
 export default class Detail extends LightningElement {
-    @api contact;
+    contact;
+    @wire(MessageContext) messageContext;
+
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+          this.messageContext,
+          CONTACT_SELECTED_CHANNEL,
+          (message) => {this.contact = message.contact}
+        );
+    }
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
 }
--- force-app/main/default/lwc/tile/tile.js
+++ force-app/main/default/lwc/tile/tile.js
@@ -1,12 +1,15 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import CONTACT_SELECTED_CHANNEL from '@salesforce/messageChannel/Contact_Selected__c';
 
 export default class Tile extends LightningElement {
     @api contact;
-
+    @wire(MessageContext) messageContext;
+    
     tileClick() {
-        const event = new CustomEvent('tileclick', {
-            detail: this.contact,
-        });
-        this.dispatchEvent(event);
+        const payload = { 
+            contact: this.contact,
+          };
+          publish(this.messageContext, CONTACT_SELECTED_CHANNEL, payload);
     }
 }
