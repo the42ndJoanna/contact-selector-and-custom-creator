ca1117fcaab0793e0a722cf5ea7f25d0877d631b
Zhihan Huang <thoughtworks.huang@mercedes-benz.com>
2024-05-02 20:09:28 +0800

    update detail skeleton

--- force-app/main/default/lwc/detail/detail.js
+++ force-app/main/default/lwc/detail/detail.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Detail extends LightningElement {}
+export default class Detail extends LightningElement {
+    @api contact;
+}
