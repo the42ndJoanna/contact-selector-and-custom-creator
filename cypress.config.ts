a5a3c9c2465df58922be77eacd51196f10f0ae38
Zhihan Huang <thoughtworks.huang@mercedes-benz.com>
2024-05-13 19:36:00 +0800

    add comments to help understanding config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,14 +3,14 @@ import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
-export default defineConfig({
+export default defineConfig({ // set up Node.js events for Cypress, here we add preprocessor plugins to Cypress and then return the configuration object
   e2e: {
-    specPattern: "**/*.feature", // 指定使用 .feature 文件
+    specPattern: "**/*.feature", // specify the pattern of test files to execute
     async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
+      await addCucumberPreprocessorPlugin(on, config); // preprocess Cucumber test files
 
-      on("file:preprocessor", createBundler({
-        plugins: [createEsbuildPlugin(config)]
+      on("file:preprocessor", createBundler({ // preprocess JavaScript files
+        plugins: [createEsbuildPlugin(config)] 
       }));
 
       return config;
